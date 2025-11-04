615ed5cf7d1952e4662138ae31e784e6c3435e78
Jean Celin <jean_estudos@hotmail.com>
2025-11-04 15:46:57 -0300

    add navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
-    <nav>
-      <div>
+    <nav className="flex  w-full place-content-between  p-4 border-b border-black/10">
+      <div className="flex  items-center  flex-row gap-2  ">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -20,16 +20,17 @@ export default function Navbar() {
           <path d="M12 3v18"></path>
           <path d="M3 7h2c2 0 5-1 7-2 2 1 5 2 7 2h2"></path>
         </svg>
+
         <span>Calculadora de Recisão</span>
       </div>
-      <div>
+      <div className="hidden">
         <span>Início</span>
         <span>Sobre</span>
         <span>FAQ</span>
         <span>Contato</span>
       </div>
       <div>
-        <button>Calcular Agora</button>
+        <button className="btn btn-primary text-nowrap">Calcular Agora</button>
       </div>
     </nav>
   );
